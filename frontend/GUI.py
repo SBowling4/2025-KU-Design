729350e9e24ac5286461a3b9dbcb1c5b60aa0fc4
SBowling4 <bowlingmsam@gmail.com>
2025-09-26 11:30:39 -0500

    Reset images to buttons, added label

--- frontend/GUI.py
+++ frontend/GUI.py
@@ -26,11 +26,9 @@ class App(TKMT.ThemedTKinterFrame):
         self.set_title()
         self.set_inputs()
         self.set_font_dropdown()
-        self.set_frame_images()
-        self.set_frame_selector()
+        self.set_frame_buttons()
         self.set_save_button()
-        self.set_background_images()
-        self.set_background_selector()
+        self.set_background_buttons()
         self.set_generate_button()
 
         for child in self.root.winfo_children():
@@ -57,7 +55,11 @@ class App(TKMT.ThemedTKinterFrame):
         font_entry = self.OptionMenu(font_option_list, self.font_var)
         font_entry.grid(column=3, row=1)
 
-    def set_frame_images(self):
+    def set_frame_buttons(self):
+        frame_label = self.Label("Frames")
+
+        frame_label.grid(column=1, row=2)
+
         frame_1_rgb = resource_images.frame_1.convert('RGB').resize((100, 100), Image.Resampling.LANCZOS)
         frame_2_rgb = resource_images.frame_2.convert('RGB').resize((100, 100), Image.Resampling.LANCZOS)
         frame_3_rgb = resource_images.frame_3.convert('RGB').resize((100, 100), Image.Resampling.LANCZOS)
@@ -66,22 +68,19 @@ class App(TKMT.ThemedTKinterFrame):
         self.images['frame_2'] = ImageTk.PhotoImage(frame_2_rgb)
         self.images['frame_3'] = ImageTk.PhotoImage(frame_3_rgb)
 
-        frame_1_label = Label(self.root, image=self.images['frame_1'])
-        frame_2_label = Label(self.root, image=self.images['frame_2'])
-        frame_3_label = Label(self.root, image=self.images['frame_3'])
+        frame_1_button = Button(self.root, command=hi, image=self.images['frame_1'])
+        frame_2_button = Button(self.root, command=hi, image=self.images['frame_2'])
+        frame_3_button = Button(self.root, command=hi, image=self.images['frame_3'])
 
-        frame_1_label.grid(column=1, row=2)
-        frame_2_label.grid(column=1, row=3)
-        frame_3_label.grid(column=1, row=4)
+        frame_1_button.grid(column=1, row=3)
+        frame_2_button.grid(column=1, row=4)
+        frame_3_button.grid(column=1, row=5)
 
-    def set_frame_selector(self):
-        frame_option_list = ['Frame 1', 'Frame 2', 'Frame 3']
-        self.frame_var.set(frame_option_list[0])
+    def set_background_buttons(self):
+        bg_label = self.Label("Backgrounds")
 
-        frame_entry = self.OptionMenu(frame_option_list, self.frame_var)
-        frame_entry.grid(column=2, row=2)
+        bg_label.grid(column=3, row=2)
 
-    def set_background_images(self):
         bg_1_rgb = resource_images.bg_1.convert('RGB').resize((100, 100))
         bg_2_rgb = resource_images.bg_2.convert('RGB').resize((100, 100))
         bg_3_rgb = resource_images.bg_3.convert('RGB').resize((100, 100))
@@ -90,30 +89,22 @@ class App(TKMT.ThemedTKinterFrame):
         self.images['bg_2'] = ImageTk.PhotoImage(bg_2_rgb)
         self.images['bg_3'] = ImageTk.PhotoImage(bg_3_rgb)
 
-        bg_1_label = Label(self.root, image=self.images['bg_1'])
-        bg_2_label = Label(self.root, image=self.images['bg_2'])
-        bg_3_label = Label(self.root, image=self.images['bg_3'])
+        bg_1_button = Button(self.root, command=hi, image=self.images['bg_1'])
+        bg_2_button = Button(self.root, command=hi, image=self.images['bg_2'])
+        bg_3_button = Button(self.root, command=hi,image=self.images['bg_3'])
 
-        bg_1_label.grid(column=3, row=2)
-        bg_2_label.grid(column=3, row=3)
-        bg_3_label.grid(column=3, row=4)
-
-    def set_background_selector(self):
-        bg_option_list = ['Background 1', 'Background 2', 'Background 3']
-
-        self.bg_var.set(bg_option_list[0])
-
-        bg_entry = self.OptionMenu(bg_option_list, self.bg_var)
-        bg_entry.grid(column=4, row=2)
+        bg_1_button.grid(column=3, row=3)
+        bg_2_button.grid(column=3, row=4)
+        bg_3_button.grid(column=3, row=5)
 
 
     def set_save_button(self):
         save_button = self.Button(text="Save Image", command=hi)
-        save_button.grid(column=3, row=5)
+        save_button.grid(column=3, row=6)
 
     def set_generate_button(self):
         generate_button = self.Button(text="Generate Image", command=hi)
-        generate_button.grid(column=1, row=5)
+        generate_button.grid(column=1, row=6)
 
     def get_entries(self) -> dict[str, str]:
         return {
